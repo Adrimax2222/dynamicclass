d899b46b6fff282355e753c04f50bc5952bd5d6c
Adrimax <adrimax.dev@gmail.com>
2025-11-26 11:35:17 +0000

    pon el logo bueno, el actusal aqui tambien cuando abres alguna pestaña e

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,11 +5,13 @@ import { Toaster } from '@/components/ui/toaster';
 import { cn } from '@/lib/utils';
 import { FirebaseClientProvider } from '@/firebase';
 
+const logoSvgDataUri = "data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 375 375'%3e%3cpath fill='%235170ff' d='M332.33 140.15C316.1 80.38 267 44.7 200.98 44.7H37.5l3.23 7.4c13.85 31.75 45.2 52.28 79.86 52.29h44.51l.37-.01c1.7-.12 7.94-.5 17.32-.5 8.98 0 18.17.35 27.24 1.05l.9.06c34.73 2.74 82.31 11.82 111.72 39.5l.37.36c.16.14.31.28.46.43l14 13.93zM336.8 209.03l-.03.58c-8.84 74.47-61.43 120.73-137.26 120.73H95.23V189.34h5.27c30.54 0 55.38 24.84 55.38 55.37v24.97h46.19c22.75 0 41.9-7.48 55.4-21.61 13.74-14.39 21-35.18 21-50.13v-.86c0-19.32-4.25-35.98-12.63-49.52l-7.82-12.64 14.05 4.88c17.42 6.04 31.55 14.1 41.99 23.94 15.27 14.38 22.92 32.98 22.74 55.29z'/%3e%3c/svg%3e";
+
 export const metadata: Metadata = {
   title: 'Dynamic Class',
   description: 'Tu asistente para Dynamic Class.',
   icons: {
-    icon: '/favicon.ico',
+    icon: logoSvgDataUri,
   },
 };
 
