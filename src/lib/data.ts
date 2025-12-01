425f77ce66cd546751ff4a14ee10093e557f0267
Adrimax <adrimax.dev@gmail.com>
2025-12-01 22:12:13 +0000

    Todo perfecto, solamente una pequeña cosa que me he dejado. El nombre de

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -55,7 +55,7 @@ export const fullSchedule: Schedule = {
     { id: 'J1', subject: 'Mates', time: '08:15 - 09:10', teacher: 'Yolanda Redondo', room: 'Aula 2.11', details: 'No hay contenido disponible.' },
     { id: 'J2', subject: 'Ciencias Sociales', time: '09:15 - 10:10', teacher: 'Lucía Nervon', room: 'Aula 2.11', details: 'No hay contenido disponible.' },
     { id: 'J3', subject: 'Itinerario', time: '10:10 - 11:10', teacher: 'Varía según el itinerario', room: 'Varía según el itinerario', details: 'La asignatura y el aula dependen de la elección del alumno.' },
-    { id: 'J4', subject: 'Activitats Cíviques', time: '11:45 - 12:40', teacher: 'N/A', room: 'Aula 2.11', details: 'No hay contenido disponible.' },
+    { id: 'J4', subject: 'Activitats Cíviques', time: '11:45 - 12:40', teacher: 'Aïda Vinyals', room: 'Aula 2.11', details: 'No hay contenido disponible.' },
     { id: 'J5', subject: 'Itinerario', time: '12:45 - 13:40', teacher: 'Varía según el itinerario', room: 'Varía según el itinerario', details: 'La asignatura y el aula dependen de la elección del alumno.' },
     { id: 'J6', subject: 'Lengua Castellana', time: '13:45 - 14:40', teacher: 'Inma Ruíz', room: 'Aula 2.11', details: 'No hay contenido disponible.' },
   ],
