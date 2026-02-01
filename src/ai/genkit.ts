5c1212cdcada0df60feb7c30bb2c63da194ab2e1
Adrimax <adrimax.dev@gmail.com>
2026-02-01 13:47:38 +0000

    Try fixing this error: `Console TypeError: (0 , _genkit_ai_next__WEBPACK

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,5 +3,5 @@ import {googleAI} from '@genkit-ai/google-genai';
 import {next} from '@genkit-ai/next';
 
 export const ai = genkit({
-  plugins: [googleAI(), next()],
+  plugins: [googleAI(), next],
 });
