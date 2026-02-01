80f635b9a74fe7d762c958c52691dcc22cf78893
Adrimax <adrimax.dev@gmail.com>
2026-02-01 12:42:20 +0000

    Fix redireccion infalible

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,13 @@ const nextConfig: NextConfig = {
   async redirects() {
     return [
       {
+        // Esta regla captura TODO lo que NO sea tu dominio oficial
         source: '/:path*',
         has: [
           {
             type: 'host',
-            value: 'studio--studio-7840988595-13b35.us-central1.hosted.app',
+            // Si el valor NO es dynamicclass.app (importante el NOT aquí)
+            value: '(?<host>^(?!dynamicclass\\.app$).*)', 
           },
         ],
         destination: 'https://dynamicclass.app/:path*',
@@ -16,7 +18,6 @@ const nextConfig: NextConfig = {
       },
     ]
   },
-  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -25,24 +26,9 @@ const nextConfig: NextConfig = {
   },
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
+      { protocol: 'https', hostname: 'placehold.co', pathname: '/**' },
+      { protocol: 'https', hostname: 'images.unsplash.com', pathname: '/**' },
+      { protocol: 'https', hostname: 'picsum.photos', pathname: '/**' },
       { protocol: 'https', hostname: 'dejardines.com' },
       { protocol: 'https', hostname: 'kuali.com.mx' },
       { protocol: 'https', hostname: 'cdn.pixabay.com' },
