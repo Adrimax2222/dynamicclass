a095738e8d2abe4c4c350a254bf8e716a47d7f11
Adrimax <adrimax.dev@gmail.com>
2026-03-01 15:23:40 +0000

    Try fixing this error: `Runtime ChunkLoadError: Loading chunk app/layout

--- src/app/(forum)/forum/actualitat/page.tsx
+++ src/app/(forum)/forum/actualitat/page.tsx
@@ -86,7 +86,7 @@ export default function ActualitatPage() {
                                     <Image 
                                         src={noticia.imageUrl} 
                                         alt={noticia.title}
-                                        layout="fill"
+                                        fill
                                         className="object-cover"
                                     />
                                 </div>
